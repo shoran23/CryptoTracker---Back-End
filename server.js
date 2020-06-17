21f7ac739e802d8cd7083293d9fa1af5a53f2b67
Matt Holmberg <mholmberg13@gmail.com>
2020-06-17 14:47:47 -0500

    heroku ts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3004;
+const PORT = process.env.PORT || 3004;
 const mongoose = require('mongoose');
 const session = require('express-session');
 const cors = require('cors');
